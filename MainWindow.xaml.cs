e49ea82c5d9ab9cad8d55b40da570931e1bf81e6
jjcpacalso <jjcpacalso@uc-bcf.edu.ph>
2024-02-14 18:58:47 +0800

    Display Player Implement property change for player attributes Add location class Add location in UI

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Engine.ViewModels;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,8 +17,15 @@ namespace CC31N_TeamName;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private GameSession _gameSession;
     public MainWindow()
     {
         InitializeComponent();
+        _gameSession = new GameSession();
+        DataContext = _gameSession;
+    }
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        _gameSession.CurrentPlayer.ExperiencePoints += 10;
     }
 }
